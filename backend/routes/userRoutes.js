64cf0668577811e2b4f2f7b129affa807a07b608
Harsh-Kumar-26 <harshkumar010377@gmail.com>
2025-11-05 17:46:47 +0530

    be- userregister, login, logout

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,13 @@
-const useroute=()=>{
-    
-}
+import express from "express";
+import { registeruser, loginuser, refreshaccesstoken, logoutuser, getcurrentuser } from "../controllers/authController.js";
+import { verifyJWT } from "../middleware/verifyAuthentication.js";
+
+const useroute = express.Router();
+
+useroute.route("/register").post(registeruser);
+useroute.route("/login").post(loginuser);
+useroute.route("/refresh-token").get(refreshaccesstoken);
+useroute.route("/logout").post(verifyJWT,logoutuser);
+useroute.route("/current-user").get(verifyJWT,getcurrentuser);
+
 export default useroute;
